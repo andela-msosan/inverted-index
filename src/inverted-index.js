eac2db415ffed75c380cfc14bbce9555ea8a52fb
andela-msosan <moyosore.sosan@andela.com>
2016-11-18 13:13:19 +0100

    create inverted index class and method to check valid json onject

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,15 +1,6 @@
 "use strict"
-var books = [{
-    "title": "Alice in Wonderland",
-    "text": "Alice falls into a rabbit hole ring and enters a world full of imagination."
-  },
 
-  {
-    "title": "The Lord of the Rings: The Fellowship of the Ring.",
-    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
-  }];
-
-class Index {
+class InvertedIndex {
   constructor() {
     this.indexes = {};
     }
@@ -30,76 +21,5 @@ class Index {
       });
       return true;
     }
-
-  /**
-   * getToken
-   * @param {String} text strings of texts
-   * @return {Array} An array of splitted texts
-   */
-    getToken(text) {
-      const invalid = /[^\w\s]/g;
-      return text.replace(invalid, " ").toLowerCase()
-      .split(" ")
-      .filter((item)=>{
-        return Boolean(item)
-      });
-    }
-
-    /**
-     * createIndex
-     * @param {JSON} fileContent Uploaded json file content
-     * @return {Object} words in file with thier index
-     */
-    createIndex(fileContent) {
-      if (this.isValidJson(fileContent)) {
-        fileContent.forEach((doc, docIndex)=>{
-          let newString = `${doc.title} ${doc.text}`
-          let tokenArray = this.getToken(newString)
-           tokenArray.forEach((token)=>{
-            if (token in this.indexes) {
-              if (this.indexes[token].indexOf(docIndex) === -1){
-                this.indexes[token].push(docIndex)
-              }
-            // this.indexes[token].push(docIndex)
-            } else
-            {
-            this.indexes[token] = [docIndex]
-            }
-          })
-       });
-      }
-    }
     
-    /**
-     * getIndex
-     * @return {Object} An object of each word and their indexex
-     */
-    getIndex() {
-      return this.indexes
-    }
-
-    /**
-     * searchIndex
-     * @param {String} word Word to be searched in the index
-     * @return {Array} description
-     */
-    searchIndex(word) {
-      const cleanWord = this.getToken(word);
-      console.log(cleanWord);
-      cleanWord.forEach((word)=>{
-        // console.log(word)
-        // console.log(this.indexes)
-        if(word in this.indexes) {
-        return this.indexes[word]
-      } else {
-        return "No result found"
-      }
-      })
-      
-
-    }
-
 }
-let ind = new Index()
-console.log(typeof (ind.createIndex(books)));
-console.log(ind.searchIndex("alice@.     in wonderland"))
