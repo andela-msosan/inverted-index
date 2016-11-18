345db3025b7299adbc67707dc2212305d5c36344
andela-msosan <moyosore.sosan@andela.com>
2016-11-18 12:34:55 +0100

    re-write test suites and cases correctly

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -12,60 +12,34 @@ const book = [
   }
 ]
 
+describe("Inverted Index", ()=> {
+  const inverted = new InvertedIndex();
+  inverted.createIndex(book);
 
-describe("Read Book Data", function() {
-  beforeEach(function () {
-    this.inverted = new Index();
-    // this.index = this.inverted.Index(book);
+  describe("Read Book Data", ()=> {
+    it("should check that uploaded file content is a valid json file", ()=> {
+      expect(inverted.isValidJson(book)).toBeTruthy();
+    });
+
+    it("should read json file and assert it is not empty", ()=> {
+      expect(inverted.isValidJson([])).toBeFalsy();
+    });
   });
 
-    describe("Json is valid", ()=> {
-      it("should check that uploaded file content is a valid json file", function() {
-        expect(this.inverted.isValidJson(book)).toBeTruthy();
-      });
+  describe("Populate Index", ()=> {
+    it("should verify that index has been created", ()=> {
+      expect(Object.keys(inverted.indexes).length).toBeGreaterThan(0);
     });
-
-    describe("Json not empty", ()=> {
-      it("should see that the uploaded file is not empty", function() {
-        expect(this.inverted.isValidJson([])).toBeFalsy();
-      });
+    it("should check that index maps the string to the correct objects in json array", ()=> {
+      expect(inverted.getIndex()["alice"]).toEqual([0]);
     });
+  });  
 
-});
-
-
-describe("Populate Data", function() {
-  beforeEach(function () {
-    this.inverted = new Index();
-    // this.index = this.inverted.Index(book);
+  describe("Search Index", ()=> {
+    it("should return an arrray of objects indexes of the searched words", ()=> {
+      expect(inverted.searchIndex("of")).toEqual([[0,1]]);
+      expect(inverted.searchIndex("alice powerful is")).toEqual([[0],[1]]);
+    });
   });
 
-    describe("Ensures that index is created once json file is read", ()=> {
-      it("should return an object once index is created", function() {
-        expect(typeof this.inverted.getIndex()).toBe("object");
-      });
-    });
-    
-    describe("Ensures that string object is mapped to the correct index", ()=> {
-      it("should return index of the object string", function() {
-        let index = this.inverted.createIndex(book);
-        expect(index.alice).toEqual([0]);
-      });
-    });
-
-});
-
-describe("Search Index", function() {
-  beforeEach(function () {
-    this.inverted = new Index();
-    // this.index = this.inverted.Index(book);
-  });
-
-    describe("Ensures that search return the correct indices of the object", ()=> {
-      it("should return indeces of the object", function() {
-        let index = this.inverted.createIndex(book);
-        expect(this.inverted.searchIndex("of")).toEqual([0]);
-      });
-    });
-
-});
+})
