b915f8f7aee2637e9bea2af63327843625cf90f2
andela-msosan <moyosore.sosan@andela.com>
2016-12-03 07:54:20 +0100

    implement angular to link to html

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,43 @@
 /* eslint-disable */
 angular.module('myApp', [])
-  .controller('mainController', ($scope) => {
+angular.module('myApp', ['toastr'])
+  .controller('mainController', ($scope, toastr) => {
     $scope.fileContent = null;
     $scope.myIndex = null;
+    $scope.count = [];
+    $scope.filesCount = []
+    $scope.indexedFiles = null;
     const myInverted = new InvertedIndex();
     $scope.showTable = false;
+    $scope.titles = [];
+    $scope.uploadedFiles = [];
 
     /**
      * Reads uploaded file content
      */
-    const fileInput = document.getElementById('file-input');
-    fileInput.addEventListener('change', () => {
-      const fileLoaded = (fileInput.files[0]);
-      const jsonFileType = 'application/json';
-      const userFileType = fileLoaded.type;
-      const isValidType = Object.is(userFileType, jsonFileType);
-      if (!isValidType) {
-        $scope.message('Invalid File Type!');
-      } else {
-        const readFile = new FileReader();
-        readFile.onload = () => {
-          const fileContent = JSON.parse(readFile.result);
-          $scope.message('File Uploaded!');
-          $scope.$apply(() => {
-            $scope.fileContent = fileContent;
-          });
-        };
-        readFile.readAsText(fileLoaded);
-      }
-    });
+    document.getElementById("file-input")
+      .addEventListener('change', (e) => {
+        loadedFiles = Array.from(e.target.files);
+        loadedFiles.forEach(file => {
+           reader = new FileReader();
+           reader.onload = (e) => {
+             $scope.$apply(() => {
+               try {
+              const fileContent = JSON.parse(e.target.result)
+              $scope.uploadedFiles[file.name] = fileContent
+              $scope.titles.push(file.name)
+              $scope.filesCount[file.name] = fileContent.length;
+              toastr.success('file Uploaded');
+               }
+               catch(err) {
+                 toastr.error("check your file")
+                 return false;
+               }
+             })
+           }
+           reader.readAsText(file);
+        });
+      })
 
     $scope.message = (message) => {
       Materialize.toast(message, 1000);
@@ -40,8 +49,8 @@ angular.module('myApp', [])
      * @param {Object} content Created file index
      * @return {Boolean} True if not valid
      */
-    $scope.checkValid = (content) => {
-      if (content.hasOwnProperty(undefined)) {
+    $scope.checkInvalid = (content) => {
+        if (content.hasOwnProperty(undefined)) {
         return true;
       }
     };
@@ -51,53 +60,73 @@ angular.module('myApp', [])
      * @function createIndex
      */
     $scope.createIndex = () => {
-      if (!$scope.fileContent) {
-        $scope.message('Upload a Valid File!');
-        $scope.showTable = false;
-        return false;
-      } else {
-        myInverted.createIndex($scope.fileContent);
-        $scope.myIndex = myInverted.getIndex();
-        if (!(myInverted.isValidJson($scope.fileContent)) || $scope.checkValid($scope.myIndex)) {
-          $scope.message('Please check your file!');
-        } else {
-          $scope.getCount();
-          $scope.showTable = true;
+        const selected = document.getElementById('selected').value;
+        if (selected === 'Select a file to create index') {
+          toastr.error("Upload and/or Select a file")
+          return false;
         }
-      }
-    };
+        if(selected) {
+          const data = $scope.uploadedFiles[selected]
+          if (data.length === 0) {
+            toastr.error("Please Check Your File", "Empty File");
+            return false;
+          }
+          myInverted.createIndex(data, selected);
+          $scope.myIndex = myInverted.getIndex(selected);
+          if ($scope.checkInvalid($scope.myIndex)) {
+            toastr.error("Check your file", "Invalid File Format")
+            return false;
+          }
+          $scope.lengths = $scope.range($scope.filesCount[selected]);
+          $scope.showTable = true;
+          $scope.indexedFiles = myInverted.fileIndices;
+        }
+      };
 
     /**
      * Gets the number of books/elements in the uploaded file
      * @function getCount
      */
-    $scope.getCount = () => {
-      const arrLength = $scope.fileContent.length;
-      const count = [];
-      for (let i = 0; i < arrLength; i++) {
-        count.push(i);
-      }
-      $scope.count = count;
-    };
+$scope.range = (lengths) => {
+        let arr = []
+        for (let i = 0; i < lengths; i++) {
+            arr.push(i);
+        }
+        return arr
+      };
 
     /**
      * Searches through the created indexes for user's input
      * @function searchIndex
      * @param {text} searchWord Texts entered to be searched.
      */
-    $scope.searchIndex = (searchWord) => {
-      if ($scope.myIndex === null) {
-        $scope.message('Upload a file and create Index');
+    $scope.searchIndex = (query) => {
+        // const selected = document.getElementById('selected').value;
+        if ($scope.myIndex === null) {
+        toastr.error('Upload a file and create Index');
         return false;
+        }
+        if ((query === '') || (query === undefined)) {
+          toastr.error('Enter words to search!');
+          $scope.showTable = false;
+          return false;
+        }
+        const searched = document.getElementById('searched').value;
+        if (searched === 'Select a file to search') {
+          toastr.error("Select file to search")
+          showTable = false;
+          return false;
+        }
+        if(searched) {
+          $scope.lengths = $scope.range($scope.filesCount[searched]);
+          $scope.myIndex = myInverted.searchIndex(query, searched);
+            if (Object.keys($scope.myIndex).length === 0) {
+              toastr.error("search result not found")
+              $scope.showTable = false;
+              return false;
+            }
+          $scope.showTable = true;
+        }
       }
-      if ((searchWord === '') || (searchWord === undefined)) {
-        $scope.message('Enter words to search!');
-      }
-      $scope.myIndex = myInverted.searchIndex(searchWord);
-      if (Object.keys($scope.myIndex).length === 0) {
-        $scope.message('No result found');
-      }
-      $scope.getCount();
-      $scope.showTable = true;
-    };
+
   });
